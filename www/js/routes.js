9b0f1dc40e6a8dc7f98a6eaf3b8c1255d094a58d
Thembelani Momoti <thembelani@digital.pepkorit.com>
2015-08-20 17:07:16 +0200

    Initial Commit

---
+++ www/js/routes.js
@@ -0,0 +1,22 @@
+angular.module('starter.routes', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise('/side/home');
+
+	$stateProvider
+		.state('sidemenu', {
+			url : '/side',
+			abstract : true,
+			templateUrl : 'templates/side-menu.html'
+		})
+
+		.state('sidemenu.home',{
+			url : '/home',
+			views: {
+				//Investigate why we cannot have a simple template Url
+				'menuContent' : {
+					templateUrl : 'templates/home.html'
+				}
+			}
+		});
+});
