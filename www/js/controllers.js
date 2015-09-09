1a57e243e355f4fc69f53ab8711388cb67b286bd
Thembelani Momoti <thembelani@digital.pepkorit.com>
2015-09-09 15:30:57 +0200

    9 Sept 2015

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -29,6 +29,8 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
 				map: map,
 				title: 'Your Pos'
 			});
+
+
 			$scope.map = map;
 			console.log(map);
 		})
@@ -39,12 +41,18 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
 });
 */
 
+
+
 .controller('NavCtrl', function($scope, $state, $cordovaGeolocation) {
   var options = {timeout: 10000, enableHighAccuracy: true};
  
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- 
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 	var myLat = position.coords.latitude;
+	var myLong = position.coords.longitude
+	console.log(myLat);
+	console.log(myLong);
+    
+    var latLng = new google.maps.LatLng(myLat, myLong);
  
     var mapOptions = {
       center: latLng,
@@ -53,8 +61,19 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
     };
  
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
+ 	
+ 	google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+ 		var marker = new google.maps.Marker({
+ 			map: $scope.map,
+ 			animation: google.maps.Animation.DROP,
+ 			position : latLng
+ 		});
+ 	})
+
   }, function(error){
     console.log("Could not get location");
   });
-});
+})
+
+
+
